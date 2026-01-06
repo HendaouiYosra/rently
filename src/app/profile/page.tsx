19cdd13df2d03392f4d3a89f83b628b93dd94a85
HendaouiYosra <hendaouiyosra7@gmail.com>
2026-01-06 21:44:56 +0100

    better profile card display

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,5 +3,6 @@ import ProfileCard from "@/components/profile/ProfileCard"
 export default function Profile(){
     return(<div>
         <ProfileCard></ProfileCard>
+        
     </div>)
 }
--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -1,40 +1,43 @@
 'use client'
 import { useUser } from "@auth0/nextjs-auth0";
-const preferences=["love", "fantazy", "style"]
+import styles from "@/components/profile/ProfileCard.module.css"
+const preferences=["fantazy", "style"]
 export default function ProfileCard(){
  const {user}=useUser();
     console.log(user);
-    return (
-         <div className="w-64 rounded-xl bg-white border border-gray-200 p-4 shadow-sm">
-      <img
-        src={user?.picture}
-        alt={user?.name}
-        className="w-16 h-16 rounded-full object-cover mx-auto"
-      />
+  return  (
+    <div className="h-[100vh] overflow-y-auto overflow-x-hidden p-6 flex justify-center scrollbar-hide">
+      <div className={styles.card}>
 
-      <p className="mt-3 text-center text-sm font-semibold text-gray-900">
-        {user?.name}
-      </p>
+        <div className={styles.avatarWrap}>
+          <img
+            src={user?.picture}
+            alt={user?.name}
+            className={styles.avatar}
+          />
+        </div>
 
-      <p className="mt-1 text-center text-xs text-gray-500">
-        {user?.nickname || user?.email}
-      </p>
-<div className="text-sm text-gray-600 text-center leading-relaxed">
-  A passionate book lover who enjoys exploring new stories and ideas.
-  Reading is a daily ritual, often finishing at least one book </div>
+        <h2 className={styles.title}>{user?.name}</h2>
 
-      <div className="mt-3 flex flex-wrap justify-center gap-1">
-        {preferences.map((tag, index) => (
-          <span
-            key={index}
-            className="text-xs px-2 py-0.5 rounded-full bg-gray-100 text-gray-600"
-          >
-            {tag}
-          </span>
-        ))}
+        <p className={styles.subtitle}>
+          {user?.nickname || user?.email}
+        </p>
+
+        <p className={styles.description}>
+          A passionate book lover who enjoys exploring new stories and ideas.
+          Reading is a daily ritual, often finishing at least one book.
+        </p>
+
+        <div className={styles.tags}>
+          {preferences.map((tag) => (
+            <span key={tag} className={styles.tag}>
+              {tag}
+            </span>
+          ))}
+        </div>
+
       </div>
     </div>
-    )
-
+  );
 
 }
